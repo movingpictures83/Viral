b66e0c008c35a3962e358369dee2d085203017ab
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2020-05-12 12:43:04 -0400

    Initial commit

---
+++ getPlugins.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git clone https://github.com/movingpictures83/CubicSpline.git ../../plugins/CubicSpline
+git clone https://github.com/movingpictures83/PLSDA-Multiple.git ../../plugins/PLSDA-Multiple
+git clone https://github.com/movingpictures83/NeuralNet.git ../../plugins/NeuralNet
+git clone https://github.com/movingpictures83/RandomForest.git ../../plugins/RandomForest
+git clone https://github.com/movingpictures83/CumulativeTime.git ../../plugins/CumulativeTime
+git clone https://github.com/movingpictures83/CumulativeClassifier.git ../../plugins/CumulativeClassifier
+git clone https://github.com/movingpictures83/TimeWarp.git ../../plugins/TimeWarp
+git clone https://github.com/movingpictures83/TopGenes.git ../../plugins/TopGenes
+git clone https://github.com/movingpictures83/GeneUnify.git ../../plugins/GeneUnify
