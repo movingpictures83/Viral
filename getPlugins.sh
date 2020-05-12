477f4c0261991d630bd4846eb0f8e6bc474d0026
Trevor Cickovski <tcickovs@castalia.cs.fiu.edu>
2020-05-12 15:25:18 -0400

    Changed TopGenes to FeatureSelection

--- getPlugins.sh
+++ getPlugins.sh
@@ -7,5 +7,5 @@ git clone https://github.com/movingpictures83/RandomForest.git ../../plugins/Ran
 git clone https://github.com/movingpictures83/CumulativeTime.git ../../plugins/CumulativeTime
 git clone https://github.com/movingpictures83/CumulativeClassifier.git ../../plugins/CumulativeClassifier
 git clone https://github.com/movingpictures83/TimeWarp.git ../../plugins/TimeWarp
-git clone https://github.com/movingpictures83/TopGenes.git ../../plugins/TopGenes
+git clone https://github.com/movingpictures83/FeatureSelection.git ../../plugins/FeatureSelection
 git clone https://github.com/movingpictures83/GeneUnify.git ../../plugins/GeneUnify
